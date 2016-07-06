f8c30bac41d75d8279ea5f99e491dbe722ba3651
LiuYimin <18355530563@163.com>
2016-07-06 10:08:58 +0800

    Initial Commit

---
+++ TryGrandCenterDispatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryGrandCenterDispatch
+//
+//  Created by Liu on 16/7/6.
+//  Copyright © 2016年 Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryGrandCenterDispatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryGrandCenterDispatch
+//
+//  Created by Liu on 16/7/6.
+//  Copyright © 2016年 Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
